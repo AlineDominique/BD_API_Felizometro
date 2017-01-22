6bb181b6c3a21422ef39c49a79c938a5a9916e1f
AlineDominique <alinedomi.silva@gmail.com>
2017-01-22 15:33:23 -0200

    Atualização de Documentos

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+	//Inclui arquivos com as funções da API.
+	include('Mensagem.php');
+	include('Resposta.php');
+
+	
+	
+    //Recupera os valores da URL
+	$metodo = $_SERVER['REQUEST_METHOD'];
+	$caminho = $_SERVER['PATH_INFO'];
+	$array = explode("/",$caminho);
+	$funcao = $array[1];
+	$id = 0;
+	if(isset($array[2])){
+		$id = $array[2];
+	}
+	$resposta = array();
+	//Verifica se o método utilizado na request é POST
+	if($metodo == "POST"){	
+		//Verifica se a função informada existe.
+		if(function_exists($funcao)){
+			//Chama função.
+			$resposta = $funcao($id);
+		}
+		else{
+			//Caso a função nao exista retorna erro.
+			$resposta = mensagens(1);
+		}
+	}
+	else
+	{
+		$resposta = mensagens(1);
+	}
+    
+    //Retorna a resposta
+	header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($resposta);
+?>
